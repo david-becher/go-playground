c73f35f1bb7c0ef370f71e89a2fbaf5d1e1c984f
David Becher <david.becher@aldi-sued.com>
2023-01-24 10:52:44 +0100

    refactor: changes

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
+import "log"
+
+type city struct {
+	cityName  string
+	state     string
+	residents struct {
+		Year2021 int `json:"2021"`
+	}
+}
+
 func main() {
-	// Read JSON
-	// Print City with highest/lowest residents
-	// Sort cities
-	// Write JSON
+	fileName := "cities.json"
+	cities := jsonToStruct(fileName)
+
+	maxResidents := cityWithHighestResidents(cities)
+	log.Printf("City with highest residents: %d", maxResidents)
+
+	sortedList := sortCitiesByResidents(cities)
+	_ = sortedList
+}
+
+func jsonToStruct(fileName string) []city {
+	panic("implement me")
+}
+
+func cityWithHighestResidents(cities []city) int {
+	maxResidents := 0
+	for _, c := range cities {
+		if maxResidents < c.residents.Year2021 {
+			maxResidents = c.residents.Year2021
+		}
+	}
+	return maxResidents
+}
+
+func sortCitiesByResidents(cities []city) []city {
+	panic("implement me")
+
 }
